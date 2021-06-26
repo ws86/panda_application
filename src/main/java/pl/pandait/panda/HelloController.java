caec3ff8a5dc305eca5c39135e98470c75d53056
Wojtek <eriah86@gmail.com>
2021-06-26 09:45:13 +0000

    test

--- src/main/java/pl/pandait/panda/HelloController.java
+++ src/main/java/pl/pandait/panda/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloController{
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name",
-		required=false, defaultValue="World")
+		required=false, defaultValue="World!")
 		String name, Model model){
 		model.addAttribute("name", name);
 		return"greeting";
